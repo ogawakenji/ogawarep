6029e20fcda47ff4a1b57c7a41c834f96a740cfd
kenoga dev <dev.kenoga.owg@gmail.com>
2015-10-25 22:37:11 +0900

    WcfSample03追加

---
+++ WcfSample03/WcfSample03Host/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.ServiceModel
+
+Module Module1
+
+    Sub Main()
+
+        Using host As ServiceHost = New ServiceHost(GetType(WcfSample03Host.MyService))
+
+            host.Open()
+
+            Console.WriteLine("サービスを開始しました。")
+            Console.WriteLine("Press any key to exit...")
+            Console.ReadKey()
+
+            host.Close()
+
+        End Using
+
+    End Sub
+
+End Module
