12329c45a63301681ba10526fb25424a895b47d5
kenoga dev <dev.kenoga.owg@gmail.com>
2015-10-26 23:23:15 +0900

    コミット

---
+++ WcfSample06/WcfService1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットを使用して制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("WcfService1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard Company")>
+<Assembly: AssemblyProduct("WcfService1")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("f54db37e-2016-4e31-b9dd-3990e74e118e")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      リビジョン
+'
+' すべての値を指定するか、'*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ WcfSample06/WcfService1/Service1.svc.vb
@@ -0,0 +1,23 @@
+﻿' メモ: コンテキスト メニューの [名前の変更] コマンドを使用すると、コード、svc、および config ファイルで同時にクラス名 "Service1" を変更できます。
+' 注意: このサービスをテストするために WCF テスト クライアントを起動するには、ソリューション エクスプローラーで Service1.svc または Service1.svc.vb を選択し、デバッグを開始してください。
+Public Class Service1
+    Implements IService1
+
+    Public Sub New()
+    End Sub
+
+    Public Function GetData(ByVal value As Integer) As String Implements IService1.GetData
+        Return String.Format("You entered: {0}", value)
+    End Function
+
+    Public Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType Implements IService1.GetDataUsingDataContract
+        If composite Is Nothing Then
+            Throw New ArgumentNullException("composite")
+        End If
+        If composite.BoolValue Then
+            composite.StringValue &= "Suffix"
+        End If
+        Return composite
+    End Function
+
+End Class
